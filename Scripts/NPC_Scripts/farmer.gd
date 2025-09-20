abba6672638568d3480e9fa49a1b096be4deeadf
Jordan Mielbrecht <jordan.mielbrecht@gmail.com>
2025-09-20 16:01:06 -0700

    adding farmer script and uid?

---
+++ Scripts/NPC_Scripts/farmer.gd
@@ -0,0 +1,16 @@
+extends Sprite3D
+
+func start_dialog():
+	print("Farmer talking")
+	var resource = load("res://Dialogue/farmer_dialogue.dialogue")
+	var finishedTask = GlobalData.mistress_task_complete
+	var dialogue_line 
+	if (finishedTask):
+		print("Talking after help")
+		dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "after_help")
+	else:
+		print("Talking before help")
+		dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "start")
+	
+	# then
+	DialogueManager.show_dialogue_balloon(resource)
