8c68a29055dd091e71d5c64067e68b9cbb6e4662
Jordan Mielbrecht <jordan.mielbrecht@gmail.com>
2025-09-21 03:47:07 -0700

    hook up rest of dialogue

--- Scripts/NPC_Scripts/schoolmarm.gd
+++ Scripts/NPC_Scripts/schoolmarm.gd
@@ -1,16 +1,33 @@
 extends Sprite3D
 
+
+var has_interacted: bool
+var count: float
+
+
+func _ready():
+	has_interacted = false
+
 func start_dialog():
-	print("Farmer talking")
+	if (DialogueManager.dialogue_started):
+		GlobalData.talking_to_npc = true
+		
+	print("Lady talking")
+	
 	var resource = load("res://Dialogue/schoolmarm_dialogue.dialogue")
 	var finishedTask = GlobalData.schoolmarm_task_complete
 	var dialogue_line 
+	var section = ""
 	if (finishedTask):
 		print("Talking after help")
-		dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "after_help")
+		section = "after_help"
+	elif (has_interacted):
+		print("interlude")
+		section = "in_between"
 	else:
 		print("Talking before help")
-		dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "start")
+		section = "start"
+		has_interacted = true
 	
 	# then
-	DialogueManager.show_dialogue_balloon(resource)
+	DialogueManager.show_dialogue_balloon(resource, section)
--- Scripts/NPC_Scripts/sheriff.gd
+++ Scripts/NPC_Scripts/sheriff.gd
@@ -1,17 +1,33 @@
 extends Sprite3D
 
+
+var has_interacted: bool
+var count: float
+
+
+func _ready():
+	has_interacted = false
+
 func start_dialog():
-	print("Farmer talking")
+	if (DialogueManager.dialogue_started):
+		GlobalData.talking_to_npc = true
+		
+	print("Lady talking")
+	
 	var resource = load("res://Dialogue/sheriff_dialogue.dialogue")
-	var finishedTask = GlobalData.farmer_task_complete && GlobalData.mayor_task_complete && GlobalData.mistress_task_complete && GlobalData.undertaker_task_complete
+	var finishedTask = GlobalData.schoolmarm_task_complete && GlobalData.farmer_task_complete && GlobalData.mayor_task_complete && GlobalData.mistress_task_complete && GlobalData.undertaker_task_complete
 	var dialogue_line 
+	var section = ""
 	if (finishedTask):
 		print("Talking after help")
-		# unlike other  NPCs, this dialogue is gonna set up the showdown with Johnny Salami
-		dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "after_help")
+		section = "after_help"
+	elif (has_interacted):
+		print("interlude")
+		section = "in_between"
 	else:
 		print("Talking before help")
-		dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "start")
+		section = "start"
+		has_interacted = true
 	
 	# then
-	DialogueManager.show_dialogue_balloon(resource)
+	DialogueManager.show_dialogue_balloon(resource, section)
