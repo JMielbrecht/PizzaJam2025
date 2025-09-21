5e59e7896154f34b546d7bfdd09fb8825c1f578f
Jordan Mielbrecht <jordan.mielbrecht@gmail.com>
2025-09-21 02:27:01 -0700

    add blacksmith and salesman dialogues

---
+++ Scripts/NPC_Scripts/blacksmith.gd
@@ -0,0 +1,20 @@
+extends Sprite3D
+
+var has_interacted: bool
+var count: float
+
+func _ready():
+	has_interacted = false
+	count = 0
+
+func start_dialog():
+	var resource = load("res://Dialogue/blacksmith_dialogue.dialogue")
+	var section
+	if (has_interacted): 
+		section = "random"
+	else:
+		section = "start"
+	#var dialogue_line = await DialogueManager.get_next_dialogue_line(resource, section)
+	
+	# then
+	DialogueManager.show_dialogue_balloon(resource, section)
